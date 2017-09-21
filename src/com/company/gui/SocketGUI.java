0479f1c80c8a0f0739386fac687a7e3e9d66f162
jibauch94 <jib090994@gmail.com>
2017-09-21 09:41:39 +0200

    gui vises når SocketGui køres - ingen logik endnu

---
+++ src/com/company/gui/SocketGUI.java
@@ -0,0 +1,23 @@
+package com.company.gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SocketGUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("SocketGUI_fxml.fxml"));
+        primaryStage.setTitle("Socket GUI");
+        primaryStage.setScene(new Scene(root, 640, 460));
+        primaryStage.show();
+
+    }
+}
