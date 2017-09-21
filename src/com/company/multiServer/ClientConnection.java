933e6cf82f2612a3f0a49da2fc8799a891f393e4
AnthonMS <Anthon@Steiness.info>
2017-09-21 12:03:38 +0200

    You can now enter NAME: name and set your name as something. Default name is Guest.

--- src/com/company/multiServer/ClientConnection.java
+++ src/com/company/multiServer/ClientConnection.java
@@ -6,11 +6,16 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.SocketException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ClientConnection implements Runnable
 {
+    private boolean done;
     private Socket socket;
+    private String name = "Guest";
+    public static ArrayList<String> nameArray = new ArrayList<String>();
+    public static ArrayList<String> messageArray = new ArrayList<String>();
 
     public ClientConnection(Socket s) throws SocketException, IOException
     {
@@ -35,7 +40,7 @@ public class ClientConnection implements Runnable
                 writeClient.println("Welcome!");
 
                 // Now we are gonna handle if the user want's to close the connection
-                boolean done = false;
+                done = false;
                 while (!done && in.hasNextLine())
                 {
                     String stream = in.nextLine();
@@ -43,12 +48,28 @@ public class ClientConnection implements Runnable
                     {
                         done = true;
                     }
-                    else
+                    else if (stream.contains("NAME:"))
                     {
                         System.out.println("RECIEVED: " + stream);
-                        writeClient.println("SERVER: " + stream);
+                        String tempName = stream.substring(5);
+                        tempName = trimCustom(tempName);
+                        name = tempName;
+                        System.out.println("Name sat as " + name);
+                        writeClient.println("SERVER: Your name is now: " + name);
+
+                    }
+                    else if (stream.contains("PUT:"))
+                    {
+
+                    }
+                    else
+                    {
+                        System.out.println("RECIEVED: " + stream + " - FROM " + name);
+                        writeClient.println(name + ": " + stream);
                     }
                 }
+
+
             }
             finally {
                 socket.close();
@@ -57,5 +78,22 @@ public class ClientConnection implements Runnable
         catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+
+    private void PUTmethod(String stream)
+    {
+
+    }
+
+
+
+    public static String trimCustom(final String s) {
+        final StringBuilder sb = new StringBuilder(s);
+        while (sb.length() > 0 && Character.isWhitespace(sb.charAt(0)))
+            sb.deleteCharAt(0); // delete from the beginning
+        while (sb.length() > 0 && Character.isWhitespace(sb.charAt(sb.length() - 1)))
+            sb.deleteCharAt(sb.length() - 1); // delete from the end
+        return sb.toString();
     }
 }
